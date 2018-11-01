96bb91ac486e391b4e844990fe6a507926b5a638
Fernando <ferry.fernando@gmail.com>
2018-11-01 14:53:46 +0700

    City search form worked.

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl } from '@angular/forms'
+import { debounceTime } from 'rxjs/operators'
+import { WeatherService } from '../weather/weather.service'
 
 @Component({
   selector: 'app-city-search',
@@ -9,7 +11,19 @@ import { FormControl } from '@angular/forms'
 export class CitySearchComponent implements OnInit {
   search = new FormControl()
 
-  constructor() {}
+  constructor(private weatherService: WeatherService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue: string) => {
+      if (searchValue) {
+        const userInput = searchValue.split(',').map(s => s.trim())
+        this.weatherService
+          .getCurrentWeather(
+            userInput[0],
+            userInput.length > 1 ? userInput[1] : undefined
+          )
+          .subscribe(data => console.log(data))
+      }
+    })
+  }
 }
