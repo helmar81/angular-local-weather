4ca5cd3b340b2c50c68b799df9c77d600ade2bc1
Fernando <ferry.fernando@gmail.com>
2018-11-01 10:50:03 +0700

    Current weather component.

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-weather',
-  templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  template: `
+    <p>current-weather works</p>
+  `,
+  styleUrls: ['']
 })
 export class CurrentWeatherComponent implements OnInit {
 
